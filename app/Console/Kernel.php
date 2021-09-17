047e6d7d54b70e91c618940dd75d61fddc332cdd
heru.handoko <heru.handoko@omni-hospitals.com>
2021-09-17 16:55:00 +0700

    210917 add scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,26 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        // $schedule->command('passport:purge')->everyMinute();
+        // $schedule->command('passport:purge')->hourly();
+
+        $schedule->call(function () {
+            DB::table('api_logs')
+                ->where('created_at', '<', Carbon::now()->subDays(65))
+                ->delete();
+        })->daily()
+            ->appendOutputTo(storage_path('logs/scheduler.log'));
+
+        // $schedule->command('passport:purge')
+        //     ->dailyAt('07:00')
+        //     ->appendOutputTo(storage_path('logs/scheduler.log'));
+
+        $schedule->call(function () {
+            DB::table('oauth_access_tokens')
+                ->where('expires_at', '<', Carbon::now()->subDays(1))
+                ->delete();
+        })->dailyAt('07:00')
+            ->appendOutputTo(storage_path('logs/scheduler.log'));
     }
 
     /**
@@ -34,7 +54,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
--- database/seeders/UserAdminSeeder.php
+++ database/seeders/UserAdminSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UserKokattoSeeder extends Seeder
+class UserAdminSeeder extends Seeder
 {
     /**
      * Run the database seeds.
